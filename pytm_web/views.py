7fb0974a8b7c25a4c11be4b26f66d07820f0c447
mirarj <44106058+mirarj@users.noreply.github.com>
2023-12-06 13:10:58 -0500

    edited views.py

--- pytm_web/views.py
+++ pytm_web/views.py
@@ -19,37 +19,22 @@ def upload_file(request):
         
         # this is a BytesIO stream
         # equivalently, this is a .py file containing the instructions to
-        #   create a TM object in pytm
+        # create a TM object in pytm
         tm_file = request.FILES['inputFile'].file
         DIR = os.path.dirname(__file__)
         with open(os.path.join(DIR, 'scripts', 'tm.py'), "wb") as f:
             f.write(tm_file.read())
+    
+        # run /mnt/c/Users/miraj/Desktop/study/capstone/ntmt_github/pytm_web/scripts/tm.py --dfd | dot -Tpng -o sample.png
         
-        from .scripts.tm import out_tm
-        # NOTE: for the time being, the TM object you create in your uploaded
-        # .py file MUST BE CALLED out_tm EXACTLY to be recognized by our
-        # prototype. TODO: how can we change this?
-
+        ps = subprocess.Popen((os.path.join(DIR, 'scripts', 'tm.py'), '--dfd'), stdout=subprocess.PIPE)
+        output = subprocess.check_output(('dot', '-Tpng', '-o', os.path.join(DIR, 'static', 'out.png')), stdin=ps.stdout)
         svg = out_tm.dfd()
-        print(type(svg))
-        
-        testdot = "strict graph {\n a -- b \n  a -- b \n  b -- a [color=blue]}"
 
-        with open(os.path.join(DIR, 'static', 'dfd.dot'), "w") as f:
-            f.write(svg)
-            
-        # (graph,) = pydot.graph_from_dot_file(os.path.join(DIR, 'static', 'dfd.dot'))
-        graphs = pydot.graph_from_dot_data(svg)
-        graphs.write_png('somefile.png')
-
-        
-
-        # (graph, ) = pydot.graph_from_dot_file(out_tm.process().dfd())
-        # graph.write_png(os.path.join(DIR, 'static', 'tm.png'))
         
         if form.is_valid():
             # process file
-            return render(request, 'pytm_web/upload.html', {'form': form, 'svg': svg})
+            return render(request, 'pytm_web/upload.html', {'form': form})
     else:
             # if request method is not POST, make an empty form
         form = UploadDFDFileForm()
