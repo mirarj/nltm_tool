2e4494fffb7b7e5779d7866e90b772cc47d7e8ec
mirarj <44106058+mirarj@users.noreply.github.com>
2023-12-08 13:41:10 -0500

    hopefully working

--- pytm_web/views.py
+++ pytm_web/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from .forms import UploadDFDFileForm
 from .scripts.img_test import *
-from pytm import TM
-import pydot
+import io
+import os
 import subprocess
 
 # base editor view
 def editor(request):
-    generate() # generate random color cat picture
+    # generate() # generate random color cat picture
     return render(request, "pytm_web/editor.html")
 
 # view for uploading file
@@ -15,32 +15,42 @@ def upload_file(request):
     # checks to see if request method is POST which means form is submitted
     if request.method == 'POST':
         # request.POST has form data, request.FILE has file data
-        form = UploadDFDFileForm(request.POST, request.FILES)
+        fileform = UploadDFDFileForm(request.POST, request.FILES)
         
-        # this is a BytesIO stream
-        # equivalently, this is a .py file containing the instructions to
-        # create a TM object in pytm
-        tm_file = request.FILES['inputFile'].file
-        DIR = os.path.dirname(__file__)
-        with open(os.path.join(DIR, 'scripts', 'tm.py'), "wb") as f:
-            f.write(tm_file.read())
-    
-        # run /mnt/c/Users/miraj/Desktop/study/capstone/ntmt_github/pytm_web/scripts/tm.py --dfd | dot -Tpng -o sample.png
-        
-        ps = subprocess.Popen((os.path.join(DIR, 'scripts', 'tm.py'), '--dfd'), stdout=subprocess.PIPE)
-        output = subprocess.check_output(('dot', '-Tpng', '-o', os.path.join(DIR, 'static', 'out.png')), stdin=ps.stdout)
-        svg = out_tm.dfd()
+        # first, check that the user actually submitted something
+        if fileform.is_valid():
+            tm_file = None
 
+            # if yes...
+            # did they submit a file?
+            if 'inputFile' in request.FILES.keys():
+                # then get the file
+                tm_file = request.FILES['inputFile'].file
+            
+            # # or did they submit via the textbox?
+            else:
+                # then get the text and treat it like a file
+                tm_file = io.BytesIO(("#!/usr/bin/env python3\n" + request.POST['inputText']).encode('ascii'))
+
+            # now save whatever they gave us to our own local file...
+            DIR = os.path.dirname(__file__)
+            with open(os.path.join(DIR, 'scripts', 'tm.py'), "wb") as f:
+                f.write(tm_file.read())
         
-        if form.is_valid():
-            # process file
-            return render(request, 'pytm_web/upload.html', {'form': form})
+            # ...and run pytm on it to generate a threat model image
+            # run /mnt/c/Users/miraj/Desktop/study/capstone/ntmt_github/pytm_web/scripts/tm.py --dfd | dot -Tpng -o sample.png
+            ps = subprocess.Popen((os.path.join(DIR, 'scripts', 'tm.py'), '--dfd'), stdout=subprocess.PIPE)
+            _  = subprocess.check_output(('dot', '-Tpng', '-o', os.path.join(DIR, 'static', 'out.png')), stdin=ps.stdout)
+
+            # then render the new page
+            return render(request, 'pytm_web/upload.html', {'fileform': fileform})
+
     else:
             # if request method is not POST, make an empty form
-        form = UploadDFDFileForm()
+        fileform = UploadDFDFileForm()
             # renders template upload.html, passing in the form
             # NEED TO IMPLEMENT upload.html TEMPLATE
-    return render(request, 'pytm_web/upload.html', {'form': form})
+    return render(request, 'pytm_web/upload.html', {'fileform': fileform})
 
 ################################################################################
 
